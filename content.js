d01be00bbf825728da0f306a4b99850f17cd5a95
ahmed-debbech <debbech.ahmed@gmail.com>
2021-06-11 15:34:55 +0100

    create the loical storage

--- content.js
+++ content.js
@@ -3,21 +3,13 @@ const focuselem = document.activeElement;
 //alert("33 " + focuselem.innerHTML);
 
 var inputs = document.getElementsByTagName('input');
-var divs = document.getElementsByTagName('span');
-alert(divs.length);
+//alert(divs.length);
 
 for(var i = 0; i < inputs.length; i++) {
     if(inputs[i].type.toLowerCase() == 'text') {
         function updateValue(input) {
-            input.value = "||||e||";
+            input.value="changed!";
         }
         inputs[i].addEventListener('change', updateValue(inputs[i]));
     }
-}
-
-for(var i = 0; i < divs.length; i++){
-        function updateValue(input) {
-            input.textContent ="||||||||";
-        }
-        divs[i].addEventListener('click', updateValue(divs[i]));
 }
--- popup.js
+++ popup.js
@@ -1,5 +1,26 @@
 const chseck = document.getElementById("appl");
+const add = document.getElementById("add_new");
+const table = document.getElementById("emojis_table");
+
+const general = document.getElementById("general").value;
+//get stored data
+chrome.storage.local.get(["general"], function(items){
+    document.getElementById("general").value = items.general;
+}); 
+add.addEventListener("click", (e) => {
+    var newRow = table.insertRow();
+    var newCell = newRow.insertCell();
+    var newCell1 = newRow.insertCell();
+    var newText = document.createElement("INPUT");
+    var newText1 = document.createElement("INPUT");
+    newCell.appendChild(newText);
+    newCell1.appendChild(newText1);
+});
 const focuselem = document.activeElement;
 chseck.addEventListener("click", (e) => {
-    alert(focuselem.id);
+    alert("saving...");
+    var gen = document.getElementById("general").value;
+    chrome.storage.local.set({ 'general': gen }, function(){
+        //  Data's been saved boys and girls, go on home
+    });
 });
